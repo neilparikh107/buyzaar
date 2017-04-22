81ee98c76e6c0b88213502838eb58f3964415aa5
Chester Lam <chesterlam620@gmail.com>
2017-04-22 11:40:13 +1000

    Add order scaffold and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ActiveRecord::Base
 
   has_one :profile
   has_many :items
+  # I am on the User table, I am looking for my purchases which is on the Order table
+  # I need to lookup on the Order table and look at the buyer_id (which is a user)
+  has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
+  # I am on the User table, I am looking for my sales which is on the Order table
+  # I need to lookup on the Order table and look at the seller_id (which is a user)
+  has_many :sales, class_name: "Order", foreign_key: "seller_id"
 
   after_create :assign_role
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408042444) do
+ActiveRecord::Schema.define(version: 20170422004241) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20170408042444) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "feedback_comment"
+    t.integer  "feedback_rating"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
+    t.integer  "item_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "profiles", force: :cascade do |t|

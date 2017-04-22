e7df9763254d944fba14c9b17ec489d60050a715
Chester Lam <chesterlam620@gmail.com>
2017-04-22 16:07:11 +1000

    Add stripe payment

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'geocoder'
 #for plotting on maps
 gem 'gmaps4rails'
 
+# Add stripe payments
+gem 'stripe'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.0'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   # get 'home/index'
   root 'home#index'
+  resources :charges
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
